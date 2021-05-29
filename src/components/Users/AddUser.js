edaa517cc6aed5684fa87d80c47c5f523628fe1b
ak1484 <ankitsaharan1484@gmail.com>
2021-05-29 12:31:04 +0530

    Managing the Error State

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,12 +6,21 @@ import ErrorModel from "../UI/ErrorModel";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values)",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (non-empty values)",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -24,9 +33,18 @@ const AddUser = (props) => {
   const ageChangeHandler = (e) => {
     setEnteredAge(e.target.value);
   };
+  const errorHandler = () => {
+    setError(null);
+  };
   return (
     <div>
-      <ErrorModel title="an err" message="somthing wrong"></ErrorModel>
+      {error && (
+        <ErrorModel
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
